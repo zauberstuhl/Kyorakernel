5bf6fb56df40b9eaad67f017004237b2170683c0
hillbeast <komcomputers@gmail.com>
2011-11-10 19:51:33 -0800

    Fixed up screen. Won't bother with profile for tm801

--- kernel/include/generated/compile.h
+++
@@ -1,8 +0,0 @@
-/* This file is auto generated, version 10 */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "Kyorakernel #10 Sat Nov 5 22:35:13 PDT 2011"
-#define LINUX_COMPILE_TIME "22:35:13"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "hillbeast-ubuntu"
-#define LINUX_COMPILE_DOMAIN
-#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-188) "
