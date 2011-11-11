9bed901e1e1a1de9b7e5d006f977f99e3b5f63b1
hillbeast <komcomputers@gmail.com>
2011-11-10 19:57:14 -0800

    Readd generated

---
+++ kernel/include/generated/compile.h
@@ -0,0 +1,8 @@
+/* This file is auto generated, version 15 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "Kyorakernel #15 Sun Nov 6 01:23:59 PST 2011"
+#define LINUX_COMPILE_TIME "01:23:59"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "hillbeast-ubuntu"
+#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-188) "
