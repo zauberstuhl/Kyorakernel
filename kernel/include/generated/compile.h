0c401558d9e49b4916fc04c363b8fdfaf3da23a0
Mark Kennard <komcomputers@gmail.com>
2011-12-03 23:27:04 +1300

    File permissions but most importantly, two handy scripts for uimage extraction

--- kernel/include/generated/compile.h
+++ kernel/include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 7 */
+/* This file is auto generated, version 0 */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#7 Fri Nov 11 02:00:50 PST 2011"
-#define LINUX_COMPILE_TIME "02:00:50"
+#define UTS_VERSION "#0 Fri Nov 25 00:55:43 NZDT 2011"
+#define LINUX_COMPILE_TIME "00:55:43"
 #define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "hillbeast-ubuntu"
+#define LINUX_COMPILE_HOST "hillbeast-virtual-machine"
 #define LINUX_COMPILE_DOMAIN
 #define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-188) "
